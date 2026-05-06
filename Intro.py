b492099a7e4f76559ca50430869764462e8adaa5
manucitavm-web <manucitavm@gmail.com>
2026-05-06 13:13:45 -0500

    Adjust image width in App 2 display

--- Intro.py
+++ Intro.py
@@ -28,7 +28,7 @@ with col1:
 with col2: 
  st.subheader("App 2")
  image = Image.open('ricitos.jpg')
- st.image(image, width=110)
+ st.image(image, width=100)
  st.write("2.	Texto a audio: Cuento Ricitos de Oro") 
  url = "https://immmanuela.streamlit.app/"
  st.write(f"App 2: [Enlace]({url})")
