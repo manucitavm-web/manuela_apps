9da8b172216cfe794504ba241a2254078650cb8e
manucitavm-web <manucitavm@gmail.com>
2026-05-06 14:19:43 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -54,7 +54,7 @@ with col2:
 with col3:
  st.subheader("Aplicación 3")
  image = Image.open('traductor.jpg')
- st.image(image, width=140)
+ st.image(image, width=150)
  st.write("Traductor Audio - Texto") 
  url = "https://traductormanuela.streamlit.app/"
  st.write(f"App 3: [Enlace]({url})")
