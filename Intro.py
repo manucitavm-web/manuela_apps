aee7793e0941558add86f9a34fe150056236f39d
manucitavm-web <manucitavm@gmail.com>
2026-05-06 12:47:56 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -16,11 +16,11 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
  
- st.subheader("Conversión de texto a voz")
+ st.subheader("Intro: Mi Primera App")
  image = Image.open('txt_to_audio2.png')
  st.image(image, width=190)
  st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
- url = "https://imultimod.streamlit.app/"
+ url = "https://introduccionmanuela.streamlit.app/"
  st.write(f"Texto a voz: [Enlace]({url})")
 
  st.subheader("Reconocimiento de Objetos")
