28780af84d5bfda689e758befe5a56c98846cbc4
manucitavm-web <manucitavm@gmail.com>
2026-05-06 13:13:12 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -28,7 +28,7 @@ with col1:
 with col2: 
  st.subheader("App 2")
  image = Image.open('ricitos.jpg')
- st.image(image, width=120)
+ st.image(image, width=110)
  st.write("2.	Texto a audio: Cuento Ricitos de Oro") 
  url = "https://immmanuela.streamlit.app/"
  st.write(f"App 2: [Enlace]({url})")
